c23a4606580d66e6b2720c034c848973a146291b
David Ordás <davorpatech+git@gmail.com>
2021-09-06 01:34:56 +0200

    refactor: modularize. change echo to printf

--- scripts/color-table-256.sh
+++ scripts/color-table-256.sh
@@ -5,7 +5,9 @@
 # This script shows you a rich color table available to use your bash shell,
 # for both properties: foreground and background.
 #
-# @AUTHOR: davorpa
+# @author   davorpatech
+# @since    2020-07-28
+# @version  2021-09-03
 #
 # @SOURCE:
 #
@@ -21,50 +23,60 @@
 #   for more details.
 #
 
+set +e
 
-# Warn if know we don't support colors
-case "$TERM" in
-    xterm-color|*-256color)
-        # colored support
-        ;;
-    *)
-        # Output to error stream
-        echo >&2 -en "\e[31mWARN:\e[0m A colored terminal support is required but seems: $TERM";
-        ;;
-esac
+main() {
+    warn_colored_terminal
+    print_colored_matrix
+    print_usage_examples
+}
 
-#
-# Build matrix
-#
-for fgbg in 38 48 ; do # Foreground / Background switch
-    for color in {0..255} ; do # 256 Colors
-        # Display the color
-        printf "\e[${fgbg};5;%sm %3s \e[0m" "$color" "$color";
-        # Display 12 colors per line
-        if [ $((($color + 1) % 12)) == 4 ] ; then
-            echo "";      # New line
-        fi
+println() {
+    printf "\n"
+}
+
+warn_colored_terminal() {
+    case "$TERM" in
+        xterm-color|*-256color)
+            # colored support
+            ;;
+        *)
+            # Output to error stream
+            printf >&2 "\e[31mWARN:\e[0m A colored terminal support is required but seems: %s\n" "$TERM"
+            ;;
+    esac
+}
+
+print_colored_matrix() {
+    for fgbg in 38 48 ; do # Foreground / Background switch
+        println
+        for color in {0..255} ; do # 256 Colors
+            # Display the color
+            printf "\e[${fgbg};5;%sm %3s \e[0m" "$color" "$color";
+            # Display 12 colors per line
+            [ $(((color + 1) % 12)) == 0 ] && println
+        done
+        println
     done
-    echo "";              # New line
-done
+}
 
-#
-# Show examples with colored sintaxis
-#
-echo -en "-> USAGE FOREGROUND: \e[38;5;42mecho -e \"\e[38;5;202m";
-    echo -n "\e[38;5;"; echo -en "\e[0m\e[38;5;82m82"; echo -en "\e[0m\e[38;5;202mm";
-        echo -en "\e[38;5;205m"; echo -en "\e[38;5;202m";
-        echo -en "\e[0m\e[38;5;82m YOUR TEXT COLORED IN 82 \e[0m\e[38;5;202m";
-    echo -n "\e[0m";
-echo -e "\e[38;5;42m\"\e[0m";
-echo "";
-echo -en "-> USAGE BACKGROUND: \e[38;5;42mecho -e \"\e[38;5;202m";
-    echo -n "\e[48;5;"; echo -en "\e[0m\e[48;5;33m33"; echo -en "\e[0m\e[38;5;202mm";
-        echo -en "\e[38;5;205m"; echo -en "\e[38;5;202m";
-        echo -en "\e[0m\e[48;5;33m YOUR TEXT COLORED IN 33 \e[0m\e[38;5;202m";
-    echo -n "\e[0m";
-echo -e "\e[38;5;42m\"\e[0m";
-echo "";
+print_usage_examples() {
+    println
+    printf " -> USAGE FOREGROUND: \e[38;5;42mecho -e \"\e[38;5;202m"
+        printf "\\\e[38;5;"; printf "\e[0m\e[38;5;82m82"; printf "\e[0m\e[38;5;202mm"
+            printf "\e[38;5;205m"; printf "\e[38;5;202m"
+            printf "\e[0m\e[38;5;82m YOUR TEXT COLORED IN 82 \e[0m\e[38;5;202m"
+        printf "\\\e[0m"
+    printf "\e[38;5;42m\"\e[0m"
+    println
+    println
+    printf " -> USAGE BACKGROUND: \e[38;5;42mecho -e \"\e[38;5;202m"
+        printf "\\\e[48;5;"; printf "\e[0m\e[48;5;33m33"; printf "\e[0m\e[38;5;202mm"
+            printf "\e[38;5;205m"; printf "\e[38;5;202m"
+            printf "\e[0m\e[48;5;33m YOUR TEXT COLORED IN 33 \e[0m\e[38;5;202m"
+        printf "\\\e[0m"
+    printf "\e[38;5;42m\"\e[0m"
+    println
+}
 
-# SIGNAL SUCCESS :)
-exit 0;
+main
